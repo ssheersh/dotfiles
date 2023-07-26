1a5a3b33fcec2a8989acb06500c449b477a29570
ssheersh <ssheersh1618@gmail.com>
2023-07-26 13:44:48 +0530

    switched to nvim

---
+++ nvim/lua/ssheersh/packer.lua
@@ -0,0 +1,74 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  use {
+  	'nvim-telescope/telescope.nvim', tag = '0.1.2',
+	-- or                            , branch = '0.1.x',
+  	requires = { {'nvim-lua/plenary.nvim'} }
+}
+
+  use {
+  	"folke/tokyonight.nvim",
+	lazy=false,
+ 	priority = 1000,
+	opts = {},
+}
+
+use { "catppuccin/nvim", as = "catppuccin" }
+
+  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use('nvim-treesitter/playground')
+  use('mbbill/undotree')
+  use ('tpope/vim-fugitive')
+  use {
+  'VonHeikemen/lsp-zero.nvim',
+  branch = 'v2.x',
+  requires = {
+    -- LSP Support
+    {'neovim/nvim-lspconfig'},             -- Required
+    {                                      -- Optional
+      'williamboman/mason.nvim',
+      run = function()
+        pcall(vim.cmd, 'MasonUpdate')
+      end,
+    },
+    {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+    -- Autocompletion
+    {'hrsh7th/nvim-cmp'},     -- Required
+    {'hrsh7th/cmp-nvim-lsp'}, -- Required
+    {'L3MON4D3/LuaSnip'},     -- Required
+  }
+}
+
+  use {
+  'nvim-lualine/lualine.nvim',
+  requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+}
+
+  use {
+  "startup-nvim/startup.nvim",
+  requires = {"nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim"},
+  config = function()
+    require"startup".setup()
+  end
+}
+
+use {'ap/vim-css-color'}
+
+ use('preservim/nerdtree')
+
+ use{
+	"L3MON4D3/LuaSnip",
+	-- follow latest release.
+	tag = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+	-- install jsregexp (optional!:).
+	run = "make install_jsregexp"
+}
+end)
---
+++ nvim/lua/ssheersh/remap.lua
@@ -0,0 +1,5 @@
+vim.g.mapleader = " "
+vim.keymap.set("n","<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n","<leader>nt", vim.cmd.NERDTreeToggle)
+vim.keymap.set("n","<leader>t", vim.cmd.tabnew)
+-- :so sets the file a source
